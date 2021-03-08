35c836e360bc2e169ecb9a2759e5230a2d14fd9d
isaacsquires <isaacsquires@me.com>
2021-03-08 18:08:11 +0000

    Basic tests on the main solver

--- tests/test_taufactor.py
+++ tests/test_taufactor.py
@@ -4,22 +4,38 @@
 
 import pytest
 
-from click.testing import CliRunner
+# from click.testing import CliRunner
 
-import taufactor
+import taufactor as tau
+import tests.utils as utils
+import numpy as np
+
+#  Testing the main solver
+
+def test_solver_on_uniform_block():
+    """Run solver on a block of ones."""
+    l=20
+    img = np.ones([l,l, l]).reshape(1, l, l, l)
+    S = tau.Solver(img)
+    t = S.solve()
+    assert t==1.0
+
+def test_solver_on_empty_block():
+    """Run solver on a block of zeros."""
+    l=20
+    img = np.zeros([l,l, l]).reshape(1, l, l, l)
+    S = tau.Solver(img, iter_limit=1000)
+    t = S.solve()
+    assert t==0.0
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_solver(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    S = taufactor.Solver()
-
+def test_solver_on_strip_of_ones():
+    """Run solver on a strip of ones, 1/2 width of total"""
+    l=20
+    img = np.zeros([l,l, l]).reshape(1, l, l, l)
+    t=10
+    img[:,:,0:t,0:t]=1
+    print(img)
+    S = tau.Solver(img)
+    t = S.solve()
+    assert t==0.25
